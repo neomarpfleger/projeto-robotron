d6688b495a4634a780f644d2571429eeef2d8ae7
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-01-15 10:16:46 -0300

    15012023
    Criadas as primeiras funções.

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,32 @@
-document.querySelector("#robotron").addEventListener("click", oi)
-
-function oi (){
-    console.log("full stack")
+/*
+função nomeada.
+function nomeDaFunção (){
+    função.....
 }
+
+função sem nome.
+function (){
+    função.....
+}
+
+arrow function(função de setas)
+()=>{
+    função......
+}
+*/
+
+//document.querySelector("#robotron").addEventListener("click", oi)
+
+document.querySelector("#robotron").addEventListener("click", () =>{
+
+    console.log("full stack")
+    console.log("oi, bem vindo!")
+    
+})
+
+function oi (nome){
+    console.log("oi " + nome)
+    console.log(", bem vindo!")
+}
+
+oi()
