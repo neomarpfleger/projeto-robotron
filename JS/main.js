885cb3f853b381a6076f6c88b3e3453dba150f77
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-01-19 05:07:24 -0300

    19012023
    Feita a interação entre pecas e estatisticas.

--- JS/main.js
+++ JS/main.js
@@ -18,36 +18,8 @@ arrow function(função de setas)
 
 
 
-const controle = document.querySelectorAll("[data-controle]")
-const estatistica = document.querySelectorAll("[data-estatistica]")
-
-
-controle.forEach( (elemento)=>{
-    elemento.addEventListener("click", (evento)=>{
-        manipulaDados(evento.target.dataset.controle,evento.target.parentNode)
-        atualizaEstatisticas(evento.target.dataset.peca)
-    })
-})
-//Toda vez que for um input pegar o "value".
-//Toda vez que for um texto usar o "textContent" para pegar o texto.
-
-
-function manipulaDados(operacao, controle){
-    const peca = controle.querySelector("[data-contador]")
-
-    if(operacao === "-"){
-        peca.value = parseInt(peca.value) - 1
-    } else {
-        peca.value = parseInt(peca.value) + 1
-    }
-}
-
-function atualizaEstatisticas(peca){
-    console.log(peca)
-}
-
-
-
+const controle = document.querySelectorAll("[data-controle]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -81,3 +53,36 @@ const pecas = {
         "velocidade": -2
     }
 }
+
+
+controle.forEach( (elemento)=>{
+    elemento.addEventListener("click", (evento)=>{
+        manipulaDados(evento.target.dataset.controle,evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
+    })
+})
+//Toda vez que for um input pegar o "value".
+//Toda vez que for um texto usar o "textContent" para pegar o texto.
+
+
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca){
+    estatistica.forEach( (elemento)=>{
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        
+    })
+    
+}
+
+
+
+
