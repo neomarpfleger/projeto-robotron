243c309de304497d019fbcacf7150d87dd49db96
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-01-19 04:30:18 -0300

    18012023

--- JS/main.js
+++ JS/main.js
@@ -19,16 +19,17 @@ arrow function(função de setas)
 
 
 const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+
 
 controle.forEach( (elemento)=>{
     elemento.addEventListener("click", (evento)=>{
         manipulaDados(evento.target.dataset.controle,evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 })
 //Toda vez que for um input pegar o "value".
 //Toda vez que for um texto usar o "textContent" para pegar o texto.
-
-
 
 
 function manipulaDados(operacao, controle){
@@ -38,5 +39,45 @@ function manipulaDados(operacao, controle){
         peca.value = parseInt(peca.value) - 1
     } else {
         peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca){
+    console.log(peca)
+}
+
+
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
 }
